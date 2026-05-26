79e81ad6c9f4fe0d36b21b0004e38ffe3b2d13b2
toosik <batownikita@gmail.com>
2026-05-27 01:19:38 +0300

    fix: suppress FutureWarning in fillna

--- menu.py
+++ menu.py
@@ -38,7 +38,7 @@ def show_results():
         index='name', columns='variant_name', values=value_col,
         aggfunc='max'
     )
-    pivot = pivot.astype('Int64').astype(object).fillna('-')
+    pivot = pivot.astype('Int64').map(lambda x: x if pd.notna(x) else '-')
 
     pivot.index.name = 'Имя'
     print()
